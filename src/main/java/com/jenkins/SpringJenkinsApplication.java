7512452672e4087f84a43adfdc3bfcb518bec3ec
RGSTUDY <rgstudy2023@gmail.com>
2025-01-14 13:50:16 +0530

    second commit

--- src/main/java/com/jenkins/SpringJenkinsApplication.java
+++ src/main/java/com/jenkins/SpringJenkinsApplication.java
@@ -1,13 +1,24 @@
 package com.jenkins;
 
+import jakarta.annotation.PostConstruct;
+import org.apache.juli.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkinsApplication {
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        logger.info("Started....");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
-	}
+    }
+
+    @PostConstruct
+    public void init() {
+        logger.info("Application has been started.....");
+    }
 
 }
--- src/test/java/com/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/SpringJenkinsApplicationTests.java
@@ -1,13 +1,19 @@
 package com.jenkins;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("Tested successfully");
 	}
 
 }
