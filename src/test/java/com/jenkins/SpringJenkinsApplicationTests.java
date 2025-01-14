9ccc4a834401a9d0e90ede94bdf32b5a8336c361
RGSTUDY <rgstudy2023@gmail.com>
2025-01-14 13:55:44 +0530

    second commit

--- src/test/java/com/jenkins/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/SpringJenkinsApplicationTests.java
@@ -1,19 +1,25 @@
 package com.jenkins;
 
+import jakarta.annotation.security.RunAs;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class SpringJenkinsApplicationTests {
+public class SpringJenkinsApplicationTests {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
 
 	@Test
 	void contextLoads() {
-		logger.info("Tested successfully");
+		logger.info("Tested successfully....");
+		logger.info("WOWOWOWOWO...");
 	}
 
 }
